b9539238b3058977bd44571ee03fd3ca02b5fe4d
loi.ly@be.com.vn <loi.ly@be.com.vn>
2025-08-06 09:38:35 +0700

    fix: cervel-configuration

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:3001",
-      "http://localhost:5173",
-      "http://localhost:8080",
+      "https://vnexpress-chi.vercel.app",
     ],
     credentials: true,
     optionsSuccessStatus: 200,
